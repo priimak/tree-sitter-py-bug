5d5b7b8752cf63f146588dc3701f038536b07147
Dmitri Priimak <priimak@gmail.com>
2026-09-17 15:13:34 -0500

    Clean up

--- src/tree_sitter_py_bug/main.py
+++ src/tree_sitter_py_bug/main.py
@@ -4,24 +4,21 @@ import tree_sitter_python
 from tree_sitter import Language, Node, Parser, Query, QueryCursor
 
 PY_LANGUAGE = Language(tree_sitter_python.language())
-PYTHON_PARSER = Parser(PY_LANGUAGE)
-HIGHLIGHTER_QUERY = Query(
-    PY_LANGUAGE,
-    """
-        (call (identifier) @function_call)
-    """,
-)
 
 if __name__ == "__main__":
     text = (Path(__file__).parent.parent.parent / "app_py.txt").read_text()
     input_text = text.encode()
-    tree = PYTHON_PARSER.parse(input_text)
-    query_cursor = QueryCursor(HIGHLIGHTER_QUERY)
+    tree = Parser(PY_LANGUAGE).parse(input_text)
+    query_cursor = QueryCursor(Query(
+        PY_LANGUAGE,
+        """
+            (call (identifier) @function_call)
+        """,
+    ))
 
     matches: list[tuple[int, dict[str, list[Node]]]] = query_cursor.matches(
         tree.root_node
     )
-    matches.sort(key=lambda m: m[0])
     for _, m in matches:
         for capture_name, nodes in m.items():
             for node in nodes:
